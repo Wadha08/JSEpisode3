c5bb7e70fb5e6557d4214922ec4d11c39f0fe881
wadha08 <wadha-ra@hotmail.com>
2019-09-15 17:24:47 +0300

    EP3

--- iterators.js
+++ iterators.js
@@ -5,7 +5,7 @@
  * - Logs every element of the array
  ************************************/
 const logger = function(array) {
-  // Your code here
+    array.forEach(x => console.log(x))
 };
 
 /*************************************
@@ -20,7 +20,7 @@ const logger = function(array) {
  *   C = (F - 32) * (5/9)
  ************************************/
 const toCelsius = function(temperatures) {
-  // Your code here
+  return temperatures.map(x => (x - 32) * (5/9) )
 };
 
 /**************************************
@@ -32,7 +32,7 @@ const toCelsius = function(temperatures) {
  *   that exceed the threshhold
  ***************************************/
 const hottestDays = function(temperatures, threshhold) {
-  // Your code here
+  return temperatures.filter(temperature => temperature > (threshhold))
 };
 
 /******************************************
@@ -50,7 +50,7 @@ const hottestDays = function(temperatures, threshhold) {
  *       all previous functions
  *******************************************/
 const logHottestDays = function(temperatures, threshhold) {
-  // Your code here
+  logger(toCelsius(hottestDays(temperatures, threshhold)))
 };
 
 export { logger, toCelsius, hottestDays, logHottestDays };
